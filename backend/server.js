8f7676fdba74e07ca8778c618681e6f9d155fdc1
Faith <omolarak724@gmail.com>
2026-06-06 22:22:38 +0100

    Fix API URL

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ connectDB()
 
 const app = express()
 
+app.set("trust proxy", 1)
+
 app.use(helmet())
 app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }))
 app.use(express.json({ limit: "2mb" }))
